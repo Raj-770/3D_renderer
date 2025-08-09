12f6ab48bd755a6e77fbc1d94c38ec3d50ba5b12
Raj <rajkumarpambhar770@gmail.com>
2025-08-09 16:44:09 +0200

    chore: add basic main implemetation

---
+++ cpu_wireframing/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "ObjParser.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./framer <.obj file>\n";
+        return 1;
+    }
+
+    ObjParser parser;
+    if (!parser.load(argv[1])) {
+        std::cerr << "Failed to load OBJ file.\n";
+        return 1;
+    }
+    std::cout << "Loaded " << parser.vertices.size() << " vertices and " 
+              << parser.faces.size() << " faces with " 
+              << parser.edges.size() << " edges.\n";
+
+    return 0;
+}
